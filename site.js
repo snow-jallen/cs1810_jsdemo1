2f1d7297f325fc2d4338f149146fb30b83564c00
Alex Mickelson <alex.mickelson@snow.edu>
2023-10-03 10:00:24 -0600

    working on js

--- site.js
+++ site.js
@@ -65,23 +65,36 @@ const animals = [
                   the matriarch.`
   },
   {
-    title: "This is a Elephant",
-    image: "/images/elephant.jpg",
-    description: `According to wikipedia: Elephants are scattered throughout
-                  sub-Saharan Africa, South Asia, and Southeast Asia and are
-                  found in different habitats, including savannahs, forests,
-                  deserts, and marshes. They are herbivorous, and they stay near
-                  water when it is accessible. They are considered to be
-                  keystone species, due to their impact on their environments.
-                  Elephants have a fission-fusion society, in which multiple
-                  family groups come together to socialise. Females (cows) tend
-                  to live in family groups, which can consist of one female with
-                  her calves or several related females with offspring. The
-                  leader of a female group, usually the oldest cow, is known as
-                  the matriarch.`
+    title: "This is a Water Buffalo",
+    image: "/images/waterBuffalo.jpg",
+    description: `According to wikipedia: Water buffalo (Bubalus bubalis), also
+                  called the domestic water buffalo or Asian water buffalo, is a
+                  large bovid originating in the Indian subcontinent and
+                  Southeast Asia. Today, it is also found in Italy, the Balkans,
+                  Australia, North America, South America and some African
+                  countries.[1] Two extant types of water buffalo are
+                  recognized, based on morphological and behavioural criteria:
+                  the river buffalo of the Indian subcontinent and further west
+                  to the Balkans, Egypt and Italy and the swamp buffalo, found
+                  from Assam in the west through Southeast Asia to the Yangtze
+                  valley of China in the east.[1][2]`
   },
 ]
 
+const getCountFromQuery = () => {
+  const queryString = window.location.search
+  if (queryString.includes("count")) {
+    return parseInt(
+      queryString
+        .split("&")
+        .filter(q => q.includes("count"))[0]
+        .split("=")[1]
+    );
+  }
+  else {
+    return 5;
+  }
+}
 
 const headerHtml = `
   <head>
@@ -118,44 +131,57 @@ const getCardHtml = (animal) => `
     </div>
   </div>`
 
+const getFooterHtml = () => `
+  <footer>
+    <ul>
+      <li>Contact</li>
+      <li>About</li>
+      <li>Privacy</li>
+    </ul>
+  </footer>`
+
 const getPageHtml = () => {
 
-  const cardHtmlArray = animals.map(a => getCardHtml(a))
-  const cardsHtml = cardHtmlArray.join('')
+  const count = getCountFromQuery()
+  const cardHtmlArray = animals
+    .slice(0, count)
+    .map(a => getCardHtml(a))
+  
+  const errorMessage = "<h1>Error, your count is bad</h1>"
+  const cardsHtml = count > 5 ? errorMessage : cardHtmlArray.join('')
 
-  return `<!DOCTYPE html>
+  return `
+    <!DOCTYPE html>
     <html>
       ${headerHtml}
       <body>
         <article class="page-container">
           ${getNavHtml()}
-
           <section class="main-container">
             ${getHeaderHtml()}
-
             <main>
               <h1>Hello Class</h1>
-
               <article class="card-container">
                 ${cardsHtml}
               </article>
             </main>
           </section>
         </article>
-
-        <!--Row -->
-        <footer>
-          <ul>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Privacy</li>
-          </ul>
-        </footer>
+        ${getFooterHtml()}
       </body>
     </html>
-    `
+  `
 }
 
 document.write(
   getPageHtml()
 )
+
+
+const cardElements = Array.from(document.getElementsByClassName("card"));
+cardElements.forEach(e => e.addEventListener("click", () => {
+  console.log("clicked")
+  // e.classList.toggle("card-selected")
+  // e.style.display="none"
+  // e.style.fontSize="2em"
+}))
